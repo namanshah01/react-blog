031ddaacdc3eea56b6204cce2a32fca79d880f30
Naman Shah <nashah2001@gmail.com>
2021-06-04 21:55:58 +0530

    minor changes

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -6,7 +6,7 @@ const BlogList = ({blogs, title}) => {
 		<div className="blog-list">
 			<div style={{display:'flex', justifyContent:'space-between'}}>
 			<div><h2>{ title }</h2></div>
-			<div><a href='https://github.com/namanshah01/django-react-blog' target='_blank' rel="noreferrer">Source code</a></div>
+			<div><a href='https://github.com/namanshah01/react-blog' target='_blank' rel="noreferrer">Source code</a></div>
 			</div>
 			{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.slug}>
